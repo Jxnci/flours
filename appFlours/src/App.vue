85ec118b3088eaba73260985a63ff9e698f54cc5
Janci <urdanivia99@gmail.com>
2023-07-14 20:44:01 -0500

    front app base

---
+++ appFlours/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <HeaderApp />
+  <Hero />
+  <HelloWorld />
+  <Testimonial />
+  <Concact />
+  <FooterApp />
+</template>
+
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import HeaderApp from "./components/HeaderApp.vue";
+import Hero from "./components/Hero.vue";
+import Concact from "./components/Contact.vue";
+import Testimonial from "./components/Testimonial.vue";
+import FooterApp from "./components/FooterApp.vue";
+</script>
+
+<style scoped></style>
